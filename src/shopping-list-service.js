b76e9b0b77e8ba80b0458fdd34adf28d7faae9fc
baf62495 <baf62495@aol.com>
2020-01-01 20:05:25 -0500

    shopping list service set up and getAllItems() working and tested

---
+++ src/shopping-list-service.js
@@ -0,0 +1,9 @@
+const ShoppingListService = {
+    getAllItems(knex) {
+        return knex
+            .select('*')
+            .from('shopping_list')
+    }
+}
+
+module.exports = ShoppingListService
---
+++ test/shopping-list-service.spec.js
@@ -0,0 +1,77 @@
+const ShoppingListService = require('../src/shopping-list-service')
+const knex = require('knex')
+
+describe('Shopping List Service object', function() {
+    let db
+    let testItems = [
+        {
+            id: 1,
+            name: 'First test item!',
+            price: '6.00',
+            date_added: new Date('2020-01-22T16:28:32.615Z'),
+            checked: false,
+            category: 'Main'
+        },
+        {
+            id: 2,
+            name: 'Second test item!',
+            price: '12.00',
+            date_added: new Date('2020-01-22T16:28:32.615Z'),
+            checked: true,
+            category: 'Main'
+        },
+        {
+            id: 3,
+            name: 'Third test item!',
+            price: '21.00',
+            date_added: new Date('2020-01-22T16:28:32.615Z'),
+            checked: true,
+            category: 'Main'
+        },
+        {
+            id: 4,
+            name: 'Fourth test item!',
+            price: '4.00',
+            date_added: new Date('2020-01-22T16:28:32.615Z'),
+            checked: false,
+            category: 'Main'
+        },
+    ]
+
+    before(() => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+    })
+
+    before(() => db('shopping_list').truncate())
+
+    afterEach(() => db('shopping_list').truncate())
+
+    after(() => db.destroy())
+
+    context(`Given 'shopping_list' has data`, () => {
+        before(() => {
+            return db
+                .into('shopping_list')
+                .insert(testItems)
+        })
+
+        it(`getAllItems() resolves all items from 'shopping_list' table`, () => {
+            return ShoppingListService.getAllItems(db)
+                .then(actual => {
+                    expect(actual).to.eql(testItems)
+                })
+        })
+    })
+
+    context(`Given 'shopping_list' has no data`, () => {
+        it(`getAllItems() resolves an empty array`, () => {
+            return ShoppingListService.getAllItems(db)
+                .then(actual => {
+                    expect(actual).to.eql([])
+                })
+        })
+    })
+})
