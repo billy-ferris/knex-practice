89d7bc7a9f5b29043feae85869bb68d5a9851571
baf62495 <baf62495@aol.com>
2020-01-01 19:29:36 -0500

    blogful curriculum example

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ( 'Title 1',  '2019-01-05', 'Lorem ipsum dolor sit amet - example 1'  ),
+    ( 'Title 2',  '2019-02-23', 'Lorem ipsum dolor sit amet - example 2'  ),
+    ( 'Title 3',  '2019-03-13', 'Lorem ipsum dolor sit amet - example 3'  ),
+    ( 'Title 4',  '2019-04-21', 'Lorem ipsum dolor sit amet - example 4'  ),
+    ( 'Title 5',  '2019-05-23', 'Lorem ipsum dolor sit amet - example 5'  ),
+    ( 'Title 6',  '2019-06-23', 'Lorem ipsum dolor sit amet - example 6'  ),
+    ( 'Title 7',  '2019-07-07', 'Lorem ipsum dolor sit amet - example 7'  ),
+    ( 'Title 8',  '2019-07-14', 'Lorem ipsum dolor sit amet - example 8'  ),
+    ( 'Title 9',  '2019-07-23', 'Lorem ipsum dolor sit amet - example 9'  ),
+    ( 'Title 10', '2019-08-16', 'Lorem ipsum dolor sit amet - example 10' ),
+    ( 'Title 11', '2019-09-18', 'Lorem ipsum dolor sit amet - example 11' ),
+    ( 'Title 12', '2019-10-04', 'Lorem ipsum dolor sit amet - example 12' ),
+    ( 'Title 13', '2019-10-10', 'Lorem ipsum dolor sit amet - example 13' ),
+    ( 'Title 14', '2019-10-18', 'Lorem ipsum dolor sit amet - example 14' ),
+    ( 'Title 15', '2019-10-27', 'Lorem ipsum dolor sit amet - example 15' ),
+    ( 'Title 16', '2019-11-02', 'Lorem ipsum dolor sit amet - example 16' ),
+    ( 'Title 17', '2019-11-07', 'Lorem ipsum dolor sit amet - example 17' ),
+    ( 'Title 18', '2019-11-24', 'Lorem ipsum dolor sit amet - example 18' ),
+    ( 'Title 19', '2019-12-13', 'Lorem ipsum dolor sit amet - example 19' ),
+    ( 'Title 20', '2019-12-21', 'Lorem ipsum dolor sit amet - example 20' )
+;
